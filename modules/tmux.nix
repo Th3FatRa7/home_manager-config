b76ad70ff0e1db44afbba2cd04e241e9e8ee3a06
th3fatra7 <142910646+Th3FatRa7@users.noreply.github.com>
2026-07-30 15:39:46 -0400

    improve tmux config

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, config, ... }: {
   programs.tmux = {
     enable = true;
     prefix = "C-a";
@@ -6,10 +6,25 @@
     baseIndex = 1;
     clock24 = true;
     mouse = true;
+    escapeTime = 0;
+    terminal = "screen-256color";
     disableConfirmationPrompt = true;
     extraConfig = ''
      set -g status-bg black
      set -g status-fg white
     '';
+    plugins = with pkgs.tmuxPlugins; [
+      resurrect
+      continuum
+      {
+        plugin = tilish;
+        # TODO: nixpkgs uses and old commit of tilish that doesn't include the feature I want. I need to fix that
+        extraConfig = ''
+          set -g @tilish-default 'main-vertical'
+          set -g @tilish-remap 'Q=q' # Doesn't work
+          set -g @tilish-project '${config.home.homeDirectory}/Projects' # Doesn't work
+        '';
+      }
+    ];
   };
 }
